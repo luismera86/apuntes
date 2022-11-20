d994a9f90148739422b7e6eb1f54b1e76e4f14c5
Luis Mera <luismera86@gmail.com>
2022-11-19 21:43:34 -0300

    React: useNavigate

---
+++ React JS/React Router/useNavigate.jsx
@@ -0,0 +1,19 @@
+/* 
+El useNavigate() se usa para navegar hacia otro path de la aplicación
+
+*/
+
+const App = () => {
+  const navigate = useNavigate();
+
+  const onHandledSend = () => {
+    navigate('/home'); // Al hacer click en el botón se dirige al path indicado en es te caso es /home
+  };
+
+  return (
+    <div>
+      <button onClick={onHandledSend}>Ir al home</button>
+    </div>
+  );
+};
+export default App;
