9c79d3ab01847eb9cc48f69f38edfef38e16ec5c
Luis Mera <luismera86@gmail.com>
2022-12-27 00:28:33 -0300

    Python: sentencias if

---
+++ python/sentencia-if.py
@@ -0,0 +1,11 @@
+
+nombre = input('Ingresar nombre: ')
+apellido = input('Ingrese el apellido: ')
+
+if nombre == 'Luis' and apellido == 'Mera': 
+  print("El nombre es Luis Mera")
+elif nombre == 'Lucho':
+  print('El nombre es Lucho')
+else:
+  print("No es nombre")
+
