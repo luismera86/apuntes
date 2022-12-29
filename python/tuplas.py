fbb1d10edb7f014061e6c6ec5f3376b0d171a079
Luis Mera <luismera86@gmail.com>
2022-12-29 02:48:17 -0300

    Python: tuplas actualizado

--- python/tuplas.py
+++ python/tuplas.py
@@ -17,3 +17,28 @@ print(busqueda) # nos devuelve que se encuentra en la posición 1
 tupla = (33, 'Hola', 'Hola', [1,2,3])
 busqueda = tupla.count('Hola')
 print(busqueda) #nos devuelve el resultado 2 ya que se encuentra dos veces repetido en la tupla
+
+# A partir de una variable llamada tupla (la cuál no sabes qué contiene) debes imprimir por pantalla los siguientes apartados (usando un print() para cada apartado), de forma ordenada:
+
+# El último elemento de la tupla.
+
+# El número de elementos que tiene la tupla.
+
+# La posición donde se encuentra el número 123 de la tupla.
+
+# Una lista con los primeros tres elementos de la tupla.
+
+# El elemento que hay en la posición con índice 4 de la tupla.
+
+# El número de veces que aparece el número 4 en la tupla.
+
+# Nota: En total son 6 prints uno debajo de otro, no puedes redefinir ni modificar la tupla o el test fallará, simplemente muestra lo que se pide por pantalla.
+
+tupla = ('Hola', 100, 123, ['hola', 100])
+
+print(tupla[-1])
+print(len(tupla))
+print(tupla.index(123))
+print([tupla[:2]])
+print(tupla[4])
+print(tupla.count(4))
