4c841c02de350dbacb8b584a42d4989d782f4db1
Luis Mera <luismera86@gmail.com>
2022-12-29 22:40:38 -0300

    Python: Funciones

---
+++ python/funciones/definicion-de-funciones.py
@@ -0,0 +1,17 @@
+
+# Declaramos una función 
+
+def saludar():
+  print('Hola Luis')
+
+
+saludar() # Ejecutamos la función 
+
+def dibujar_tabla_del_5():
+  for i in range(11):
+    print('5 *', i, '=', i*5 )
+  
+
+dibujar_tabla_del_5()
+
+ 
---
+++ python/funciones/retorno-de-valores.py
@@ -0,0 +1,8 @@
+# Funciones con retorno de valores
+
+def test(): 
+  return 'Hola'
+
+variable = test() # La variable toma el valor del return de la función test
+
+print(variable) # Devuelve Hola 
