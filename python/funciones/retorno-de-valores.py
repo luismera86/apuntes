1e37b3dece63817332d60b2d34d8040158ac0404
Luis Mera <luismera86@gmail.com>
2022-12-30 00:29:24 -0300

    Python: enviando valores

--- python/funciones/retorno-de-valores.py
+++ python/funciones/retorno-de-valores.py
@@ -6,3 +6,25 @@ def test():
 variable = test() # La variable toma el valor del return de la función test
 
 print(variable) # Devuelve Hola 
+
+
+def test2(): 
+  suma = 1 + 2
+  return suma
+
+resultado = test2()
+
+print(resultado) # 3
+
+def test3():
+  return 'Hola', 20, [1,2,3]
+
+print(test3()) # Cuando la función retorna varios elementos los almacena en una tupla ('Hola', 20, [1, 2, 3])
+
+# Descomponiendo el return 
+
+valor1, valor2, valor3 = test3()
+
+print(valor1)
+print(valor2)
+print(valor3)
