e63c5754621763db81a39a56f310f558e0f84ac5
Luis Mera <luismera86@gmail.com>
2023-01-16 14:33:17 -0300

    Python: clases y herencia

---
+++ python/clases/herencias.py
@@ -0,0 +1,16 @@
+
+class Producto:
+    def __init__(self, referencia, tipo, nombre, pvp, descripcion):
+        self.referencia 
+        self.tipo 
+        self.nombre 
+        self.pvp 
+        self.descripcion
+
+class Adorno(Producto): #Ponemos entre los () la clase padre de donde va a heredar los atributos y métodos
+
+    pass
+
+class Alimento(Producto):
+    productor = ''
+    distribuido = ''
