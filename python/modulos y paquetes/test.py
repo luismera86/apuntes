bd203857b4a882e749a9bc0756ccb46a68df4294
Luis Mera <luismera86@gmail.com>
2023-01-17 01:22:34 -0300

    Python: Módulos y paquetes

---
+++ python/modulos y paquetes/test.py
@@ -0,0 +1,10 @@
+# import saludos # importamos el módulo desde otro script
+
+# saludos.saludar()
+
+from saludos import saludar, Saludo # importación selectiva del módulo
+
+saludar()
+
+saludo1 = Saludo()
+
