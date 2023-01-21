6da84a208434c9900873152f1af90fb9d2c4e76a
Luis Mera <luismera86@gmail.com>
2023-01-21 18:43:43 -0300

    Python: Ficheros de texto

--- python/manejo de ficheros/ficheros_de_texto/main.py
+++ python/manejo de ficheros/ficheros_de_texto/main.py
@@ -5,8 +5,9 @@ texto = 'Es un texto\notra linea de texto'
 fichero = open('fichero.txt', 'w') # abrimos un fichero de de texto de escritura la w significa write
 
 fichero.write(texto) # escribimos sobre el fichero
-
+print(fichero)
 fichero.close()
+
 
 fichero = open('fichero.txt', 'r')
 texto_fichero = fichero.read() # guardamos en la variable el texto del fichero
@@ -25,3 +26,8 @@ print(lineas)
 fichero = open('fichero.txt', 'a')
 
 fichero.write('\ntercera linea') # agregamos una linea nueva al fichero sin borrar las anteriores
+
+fichero.close()
+
+# La diferencia cuando abrimos un fichero con 'w' y con 'a' es que en el primero el puntero se coloca al principio del documento, es donde comienza a escribirse el fichero, en cambio cuando lo abres con 'a' el puntero se coloca al final del fichero, por lo cual la información que se escriba se va a colocar al final de todo.
+
