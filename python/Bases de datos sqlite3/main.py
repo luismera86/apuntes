16186222779045634b6926b1110ce00ec87eb622
Luis Mera <luismera86@gmail.com>
2023-02-02 23:31:48 -0300

    Python: bases de datos sqlite3

--- python/Bases de datos sqlite3/main.py
+++ python/Bases de datos sqlite3/main.py
@@ -17,12 +17,31 @@ cursor = conexion.cursor() # Iniciamos el cursor con el que vamos a ejecutar có
 
 # cursor.executemany('INSERT INTO usuarios VALUES (?,?,?)', usuarios) # Insertamos toda la lista de usuarios
 
-cursor.execute('SELECT * FROM usuarios') # leemos la información de la tabla
-usuarios = cursor.fetchall()
-print(usuarios) # [('Luis', 36, 'luismera@gmail.com'), ('Lucho', 30, 'mail1@cmail.com'), ('Maria', 50, 'mail2@cmail.com'), ('Mario', 36, 'mail3@cmail.com')]
+# cursor.execute('SELECT * FROM usuarios') # leemos la información de la tabla
+# usuarios = cursor.fetchall()
+# print(usuarios) # [('Luis', 36, 'luismera@gmail.com'), ('Lucho', 30, 'mail1@cmail.com'), ('Maria', 50, 'mail2@cmail.com'), ('Mario', 36, 'mail3@cmail.com')]
 
-for usuario in usuarios: # Hacemos un ciclo for para leer uno por uno los usuarios de la tabla
-    print(usuario)
+# for usuario in usuarios: # Hacemos un ciclo for para leer uno por uno los usuarios de la tabla
+#     print(usuario)
+
+## Claves primarias
+
+# cursor.execute('''
+#     CREATE TABLE usuarios (
+#     dni VARCHAR(9) PRIMARY KEY,
+#     nombre VARCHAR(100), 
+#     edad INTEGER, 
+#     email VARCHAR(100)
+#     )
+# ''')
+
+usuarios = [
+    ('3234932354', 'Lucho', 30, 'mail1@cmail.com'),
+    ('3234932355', 'Maria', 50, 'mail2@cmail.com'),
+    ('3234932333', 'Mario', 36, 'mail3@cmail.com'),
+]
+
+cursor.execute("INSERT INTO usuarios VALUES ('3234932358', 'Juan', 36, 'mail3@cmail.com')")
 
 conexion.commit() # este método hace que se guarden los cambios para insertarlos en la base de datos
 conexion.close()
